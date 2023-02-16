b2c95ed7954acbd710696e0ab971fd70002f2a2c
Botalov Serg <89090168690@mail>
2023-02-16 15:30:36 +0500

    реализованы хэндлеры для обработки сообщений

---
+++ handlers/user_handlers.py
@@ -0,0 +1,19 @@
+from copy import deepcopy
+
+from aiogram import Dispatcher
+from aiogram.types import CallbackQuery, Message
+
+from database.database import user_dict_template, users_db
+from keyboards.bookmarks_kb import create_bookmarks_keyboard, create_edit_keyboard
+from keyboards.pagination_kb import create_pagination_keyboard
+from lexicon.lexicon import LEXICON
+from services.file_handling import book
+
+
+# Этот хэндлер будет срабатывать на команду "/start" -
+# добавлять пользователя в базу данных, если его там еще не было
+# и отправлять ему приветственное сообщение
+async def process_start_command(message: Message):
+    await message.answer(LEXICON[message.text])
+    if message.from_user.id not in users_db:
+        users_db[message.from_user.id] = deepcopy(user_dict_template)
